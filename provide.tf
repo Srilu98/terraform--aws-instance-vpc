2a99856013c207e251cd40b698a38120992fffaa
Srilu98 <116946110+Srilu98@users.noreply.github.com>
2022-11-01 13:31:52 +0530

    Update provide.tf

--- provide.tf
+++ provide.tf
@@ -1,11 +1,13 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
+      source = "hashicorp/aws"
+      version = "4.37.0"
     }
   }
 }
+
 provider "aws" {
-  region     = "us-east-1"
+  # Configuration options
+  region = "us-east-1"
 }
